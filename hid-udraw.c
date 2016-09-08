6399d4f8e2365f48347e5cb5e3ad067ae7a247ed
Bastien Nocera <hadess@hadess.net>
2016-09-08 03:33:45 +0200

    implement joypad and touchpad

--- hid-udraw.c
+++ hid-udraw.c
@@ -38,31 +38,21 @@ MODULE_LICENSE("GPL");
  * - an optional, disabled by default, accelerometer device
  */
 
-static const unsigned short udraw_key_table[] = {
-	KEY_RESERVED,
-	KEY_MENU,
-	KEY_PLAYPAUSE,
-	KEY_FORWARD,
-	KEY_BACK,
-	KEY_VOLUMEUP,
-	KEY_VOLUMEDOWN,
-	KEY_RESERVED,
-	KEY_RESERVED,
-	KEY_RESERVED,
-	KEY_RESERVED,
-	KEY_RESERVED,
-	KEY_RESERVED,
-	KEY_RESERVED,
-	KEY_ENTER,
-	KEY_PLAYPAUSE,
-	KEY_RESERVED,
+static const unsigned short udraw_joy_key_table[] = {
+	BTN_SOUTH,
+	BTN_NORTH,
+	BTN_EAST,
+	BTN_WEST,
+	BTN_SELECT,
+	BTN_START,
+	BTN_MODE
 };
 
 struct udraw {
 	struct input_dev *joy_input_dev;
 	struct input_dev *touch_input_dev;
+	bool touch_input_dev_registered;
 	struct hid_device *hdev;
-	unsigned short keymap[ARRAY_SIZE(udraw_key_table)];
 };
 #if 0
 static int get_key(int data)
@@ -120,10 +110,81 @@ static int udraw_raw_event(struct hid_device *hdev, struct hid_report *report,
 	 u8 *data, int len)
 {
 	struct udraw *udraw = hid_get_drvdata(hdev);
+	int x, y;
 
 	if (len != 0x1B)
 		goto out;
 
+	/* joypad */
+	input_report_key(udraw->joy_input_dev, BTN_WEST, data[0] & 1);
+	input_report_key(udraw->joy_input_dev, BTN_SOUTH, data[0] & 2);
+	input_report_key(udraw->joy_input_dev, BTN_EAST, data[0] & 4);
+	input_report_key(udraw->joy_input_dev, BTN_NORTH, data[0] & 8);
+
+	input_report_key(udraw->joy_input_dev, BTN_SELECT, data[1] & 1);
+	input_report_key(udraw->joy_input_dev, BTN_START, data[1] & 2);
+	input_report_key(udraw->joy_input_dev, BTN_MODE, data[1] & 16);
+
+	//???
+	x = y = 0;
+	switch (data[2]) {
+	case 0x0:
+		y = 127;
+		break;
+	case 0x1:
+		y = 127;
+		x = 127;
+		break;
+	case 0x2:
+		x = 127;
+		break;
+	case 0x3:
+		y = -127;
+		x = 127;
+		break;
+	case 0x4:
+		y = -127;
+		break;
+	case 0x5:
+		y = -127;
+		x = -127;
+		break;
+	case 0x6:
+		x = -127;
+		break;
+	case 0x7:
+		y = 127;
+		x = -127;
+		break;
+	default:
+		;;
+	}
+
+	input_report_abs(udraw->joy_input_dev, ABS_X, x);
+	input_report_abs(udraw->joy_input_dev, ABS_Y, y);
+
+	input_sync(udraw->joy_input_dev);
+
+	/* touchpad */
+	x = y = 0;
+	/* Finger(s) in use */
+	if (data[11] == 0x80 || data[11] >= 191) {
+		if (data[15] != 0x0F && data[17] != 0xFF)
+			x = data[15] * 256 + data[17];
+		if (data[16] != 0x0F && data[18] != 0xFF)
+			y = data[16] * 256 + data[18];
+
+		input_report_key(udraw->touch_input_dev, BTN_TOUCH, 1);
+		input_report_key(udraw->touch_input_dev, BTN_TOOL_DOUBLETAP,
+				(data[11] != 0x80));
+
+		input_report_abs(udraw->touch_input_dev, ABS_X, x);
+		input_report_abs(udraw->touch_input_dev, ABS_Y, y);
+	} else {
+		input_report_key(udraw->touch_input_dev, BTN_TOUCH, 0);
+		input_report_key(udraw->touch_input_dev, BTN_TOOL_DOUBLETAP, 0);
+	}
+	input_sync(udraw->touch_input_dev);
 #if 0
 	static const u8 keydown[] = { 0x25, 0x87, 0xee };
 	static const u8 keyrepeat[] = { 0x26, };
@@ -203,7 +264,7 @@ static void udraw_close(struct input_dev *dev)
 	hid_hw_close(udraw->hdev);
 }
 
-static struct input_dev *udraw_setup_touch(struct hid_device *hdev)
+static struct input_dev *allocate_and_setup(struct hid_device *hdev)
 {
 	struct input_dev *input_dev;
 
@@ -223,25 +284,47 @@ static struct input_dev *udraw_setup_touch(struct hid_device *hdev)
 	input_dev->id.version = hdev->version;
 	input_set_drvdata(input_dev, hid_get_drvdata(hdev));
 
-	//FIXME setup the keys
+	return input_dev;
+}
+
+static struct input_dev *udraw_setup_touch(struct udraw *udraw,
+		struct hid_device *hdev)
+{
+	struct input_dev *input_dev;
+
+	input_dev = allocate_and_setup(hdev);
+	if (!input_dev)
+		return NULL;
+
+	input_dev->evbit[0] = BIT(EV_ABS) | BIT(EV_KEY);
+
+	set_bit(ABS_X, input_dev->absbit);
+	input_set_abs_params(input_dev, ABS_X, 0, 1920, 0, 0);
+	set_bit(ABS_Y, input_dev->absbit);
+	input_set_abs_params(input_dev, ABS_X, 0, 1080, 0, 0);
+
+	set_bit(BTN_TOOL_DOUBLETAP, input_dev->keybit);
+	set_bit(BTN_TOUCH, input_dev->keybit);
+
+	set_bit(INPUT_PROP_POINTER, input_dev->propbit);
+	set_bit(INPUT_PROP_BUTTONPAD, input_dev->propbit);
 
 	return input_dev;
 }
 
-static void udraw_setup_joypad(struct input_dev *input_dev)
+static void udraw_setup_joypad(struct udraw *udraw, struct input_dev *input_dev)
 {
-#if 0
-	input_dev->keycode = udraw->keymap;
-	input_dev->keycodesize = sizeof(unsigned short);
-	input_dev->keycodemax = ARRAY_SIZE(udraw->keymap);
+	int i;
 
-	input_dev->evbit[0] = BIT(EV_KEY) | BIT(EV_REP);
+	input_dev->evbit[0] = BIT(EV_KEY) | BIT(EV_ABS);
 
-	memcpy(udraw->keymap, udraw_key_table, sizeof(udraw->keymap));
-	for (i = 0; i < ARRAY_SIZE(udraw_key_table); i++)
-		set_bit(udraw->keymap[i], input_dev->keybit);
-	clear_bit(KEY_RESERVED, input_dev->keybit);
-#endif
+	set_bit(ABS_X, input_dev->absbit);
+	input_set_abs_params(input_dev, ABS_X, -127, 127, 0, 0);
+	set_bit(ABS_Y, input_dev->absbit);
+	input_set_abs_params(input_dev, ABS_Y, -127, 127, 0, 0);
+
+	for (i = 0; i < ARRAY_SIZE(udraw_joy_key_table); i++)
+		set_bit(udraw_joy_key_table[i], input_dev->keybit);
 }
 
 static int udraw_input_configured(struct hid_device *hdev,
@@ -249,20 +332,31 @@ static int udraw_input_configured(struct hid_device *hdev,
 {
 	struct input_dev *input_dev = hidinput->input;
 	struct udraw *udraw = hid_get_drvdata(hdev);
-	int i;
+	int error = 0;
 
+	/* joypad, uses the hid device */
 	udraw->joy_input_dev = input_dev;
-	udraw_setup_joypad(input_dev);
+	udraw_setup_joypad(udraw, input_dev);
 
-	udraw->touch_input_dev = udraw_setup_touch(hdev);
+	/* touchpad */
+	udraw->touch_input_dev = udraw_setup_touch(udraw, hdev);
 	if (!udraw->touch_input_dev)
 		return -1;
+	error = input_register_device(udraw->touch_input_dev);
+	if (error)
+		goto fail_register_touch_input;
+	udraw->touch_input_dev_registered = true;
+
+	/* pen */
+
+	/* accelerometer */
 
 	//FIXME
 	// udraw_setup_pen
 	// udraw_setup_accel
 
-	return 0;
+fail_register_touch_input:
+	return error;
 }
 
 static int udraw_input_mapping(struct hid_device *hdev,
@@ -313,6 +407,12 @@ static void udraw_remove(struct hid_device *hdev)
 {
 	struct udraw *udraw = hid_get_drvdata(hdev);
 	hid_hw_stop(hdev);
+	/* joy_input_device is always registered and allocated */
+	if (udraw->touch_input_dev) {
+		if (udraw->touch_input_dev_registered)
+			input_unregister_device(udraw->touch_input_dev);
+		input_free_device(udraw->touch_input_dev);
+	}
 	kfree(udraw);
 }
 
@@ -327,7 +427,7 @@ static const struct hid_device_id udraw_devices[] = {
 MODULE_DEVICE_TABLE(hid, udraw_devices);
 
 static struct hid_driver udraw_driver = {
-	.name = "udraw",
+	.name = "hid-udraw",
 	.id_table = udraw_devices,
 	.raw_event = udraw_raw_event,
 	.input_configured = udraw_input_configured,
